33f73c3396711bb03fbe6beeeecb2b5837d8c3ec
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-07-15 13:57:59 +0200

    added  gitignore

--- Workspace/latex_build.sh
+++ Workspace/latex_build.sh
@@ -1,5 +1,5 @@
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
-bibtex main
+bibtex output/main
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
 cp output/main.pdf .
