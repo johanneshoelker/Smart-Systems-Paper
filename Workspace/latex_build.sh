c7bdf0af0fbbe6e3f9758e19dc9c98298d7c4af8
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-08-01 18:29:53 +0200

    working on zotero api

--- Workspace/latex_build.sh
+++ Workspace/latex_build.sh
@@ -1,3 +1,5 @@
+pandoc -f csv -t latex -o './includings/methods.tex' '../Findings/methods.csv'
+curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/3LVTYECH/items?format=bibtex" > 'bibs/main.bib'
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
 bibtex output/main
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
