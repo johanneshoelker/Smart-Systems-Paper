609d57e94f6f7e756a7da2eb7e5a8c1013fe1b3c
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-08-04 11:28:26 +0200

    summarized debiased article

--- Workspace/latex_build.sh
+++ Workspace/latex_build.sh
@@ -1,6 +1,6 @@
 pandoc -f csv -t latex -o './includings/methods.tex' '../Findings/methods.csv'
 okular main.pdf &
-curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex" > 'bibs/main.bib'
+curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex&limit=100" > 'bibs/main.bib'
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
 bibtex output/main
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
