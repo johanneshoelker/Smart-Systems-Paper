ca499e6b42720d962eba5c8950f649109f206202
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-08-30 17:37:08 +0000

    fixed zotero pagination

--- Workspace/latex_build.sh
+++ Workspace/latex_build.sh
@@ -28,7 +28,8 @@ pandoc -f csv -t latex -o './includings/methods.tex' '../Findings/methods.csv'
 #   # Wait a short time before the next request to avoid rate limiting (optional)
 #   sleep 1
 # done
-curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex&limit=100&sort=title" > bibs/main.bib
+curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex&limit=100&sort=title"  > bibs/main.bib
+curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex&limit=100&sort=title&start=100" >> bibs/main.bib
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
 bibtex output/main
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
