adaa171ecab93a313d8e53b9d0d15a71438a566d
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-09-06 21:04:55 +0200

    added method on build script

--- Workspace/latex_build.sh
+++ Workspace/latex_build.sh
@@ -1,33 +1,7 @@
 pandoc -f csv -t latex -o './includings/methods.tex' '../Findings/methods.csv'
+sed -i "s/\bno\b/\\\\xmark/g" './includings/methods.tex'
+sed -i "s/\byes\b/\\\\cmark/g" './includings/methods.tex'
 # okular main.pdf &
-# Set your API Key, User ID, and Collection ID
-# API_KEY="SiQPnCcEMmppgThbSGuVm5Rv"
-# USER_ID="7485781"
-# COLLECTION_ID="7JIJ4G7F"
-#
-# # Initial URL to fetch the first page of results
-# URL="https://api.zotero.org/users/$USER_ID/collections/$COLLECTION_ID/items?format=bibtex&limit=100&sort=title"
-#
-# # Output file for BibTeX data
-# OUTPUT_FILE="bibs/main.bib"
-#
-# # Empty the output file initially
-# > "$OUTPUT_FILE"
-#
-# # Loop until there are no more pages
-# while [ -n "$URL" ]; do
-#   # Perform the request and capture headers and body
-#   RESPONSE=$(curl -s -D - -H "Zotero-API-Key: $API_KEY" "$URL")
-#   echo "$RESPONSE"
-#   # Extract the body (actual BibTeX data) and append it to the output file
-#   echo "$RESPONSE" | sed -n '/^{/,$p' >> "$OUTPUT_FILE"
-#
-#   # Extract the Link header to check for the next page
-#   URL=$(echo "$RESPONSE" | grep -oP '(?<=<)[^>]+(?=>; rel="next")')
-#
-#   # Wait a short time before the next request to avoid rate limiting (optional)
-#   sleep 1
-# done
 curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex&limit=100&sort=title"  > bibs/main.bib
 curl -s -H "Zotero-API-Key: SiQPnCcEMmppgThbSGuVm5Rv" "https://api.zotero.org/users/7485781/collections/7JIJ4G7F/items?format=bibtex&limit=100&sort=title&start=100" >> bibs/main.bib
 pdflatex -pdf -aux-directory=./temp -output-directory=./output main.tex
