60700df62edfd4b1d0feb7e707df9ba544d630c2
johanneshoelker <johannes.hoelker@uni-kassel.de>
2024-09-17 20:04:49 +0200

    refinement: review

--- latex_auto_build.sh
+++ latex_auto_build.sh
@@ -1,4 +1,10 @@
 cd Workspace
+
 while find . -name '*.tex' | xargs inotifywait -qqre modify .; do \
-  ./latex_build.sh; \
+  clear
+  echo "A Latex file was saved and the building process is started..."
+  ./latex_build.sh
+  clear
+  echo "Build process finished successfully."
+  echo "Waiting for new changes...."; \
 done
